8e7b99549e247ae1f731beb842ab8a7ec3c25f13
admin <vovasl2010@gmail.com>
2023-09-18 21:49:35 +0300

    disable sofa davis cup

--- backend/components/sofascore/models/TennisEvent.php
+++ backend/components/sofascore/models/TennisEvent.php
@@ -9,7 +9,7 @@ class TennisEvent
 {
 
     /** allowed tournament categories */
-    CONST TOUR = [3, 76, 72];
+    CONST TOUR = [3, 72];
 
     /** allowed status codes */
     CONST STATUS_CODES = [100, 98, 92, 91];
