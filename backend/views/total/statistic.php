267bc10b8f79d9669e5f50bb37a6710e4b0bb8e2
vovasl <vovasl2010@gmail.com>
2023-12-16 16:18:35 +0200

    total - stats

--- backend/views/total/statistic.php
+++ backend/views/total/statistic.php
@@ -1,35 +1,113 @@
 <?php
 
-
-use backend\models\statistic\FilterModel;
+use backend\models\total\StatisticTotalSearch;
 use frontend\models\sport\Odd;
-use yii\helpers\Html;
+use frontend\models\sport\Surface;
+use frontend\models\sport\Total;
 use yii\web\View;
+use yii\data\ActiveDataProvider;
+use frontend\models\sport\Tour;
+use yii\helpers\Html;
+use yii\grid\GridView;
 
 /**
  * @var View $this
- * @var array $stats
- * @var FilterModel $filter
+ * @var StatisticTotalSearch $searchModel
+ * @var ActiveDataProvider $dataProvider
  */
+
+$this->title = 'Statistic';
+
+$reset = "/total/statistic";
 
 ?>
 
-<h1 class="mb-5">Totals</h1>
+<div class="tournament-index">
 
-<?= $this->render('statistic/_filter', ['filter' => $filter]); ?>
+    <h1><?= Html::encode($this->title) ?></h1>
 
-<?= Html::a('Tournaments',
-    ['/statistic/total-tournaments', 'tour' => $filter->tour, 'surface' => $filter->surface, 'qualifier' => $filter->round],
-    ['target'=>'_blank', 'class' => 'btn btn-primary mb-4']
-)?>
+    <p>
+        <?= Html::a('Clear', [$reset], ['class' => 'btn btn-primary']) ?>
+    </p>
 
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'summary' => 'Total: {totalCount}',
+        'columns' => [
+            [
+                'label' => 'Tour',
+                'attribute' => 'tour_id',
+                'value' => '',
+                'filter' => Tour::dropdown()
+            ],
+            [
+                'label' => 'Surface',
+                'attribute' => 'surface_id',
+                'value' => '',
+                'filter' => Surface::dropdown()
+            ],
+            [
+                'label' => 'Type',
+                'attribute' => 'type',
+                'value' => 'type',
+                'filter' => [
+                    Odd::ADD_TYPE['over'] => Odd::ADD_TYPE['over'],
+                    Odd::ADD_TYPE['under'] => Odd::ADD_TYPE['under']
+                ]
+            ],
+            [
+                'label' => 'Moneyline',
+                'attribute' => 'min_moneyline',
+                'value' => '',
+            ],
+            [
+                'label' => '5 Sets',
+                'attribute' => 'five_sets',
+                'value' => '',
+                'filter' => ['No', 'Yes'],
+            ],
+            [
+                'label' => '<1.76',
+                'attribute' => 'percent_profit_0',
+                'filter' => '',
+                'value' => function(Total $model) {
+                    return "{$model->percentProfit0}({$model->count_profit_0})";
+                },
+            ],
+            [
+                'label' => '1.76-1.85',
+                'attribute' => 'percent_profit_1',
+                'filter' => '',
+                'value' => function(Total $model) {
+                    return "{$model->percentProfit1}({$model->count_profit_1})";
+                },
+            ],
+            [
+                'label' => '1.86-1.96',
+                'attribute' => 'percent_profit_2',
+                'filter' => '',
+                'value' => function(Total $model) {
+                    return "{$model->percentProfit2}({$model->count_profit_2})";
+                },
+            ],
+            [
+                'label' => '1.97-2.08',
+                'attribute' => 'percent_profit_3',
+                'filter' => '',
+                'value' => function(Total $model) {
+                    return "{$model->percentProfit3}({$model->count_profit_3})";
+                },
+            ],
+            [
+                'label' => '>=2.09',
+                'attribute' => 'percent_profit_4',
+                'filter' => '',
+                'value' => function(Total $model) {
+                    return "{$model->percentProfit4}({$model->count_profit_4})";
+                },
+            ],
+        ],
+    ]); ?>
 
-<?php foreach (Odd::ADD_TYPE as $type) : ?>
-
-    <?= $this->render('statistic/_outer', [
-        'type' => $type,
-        'stats' => $stats,
-        'detail' => 0,
-    ]) ?>
-
-<?php endforeach; ?>
+</div>
