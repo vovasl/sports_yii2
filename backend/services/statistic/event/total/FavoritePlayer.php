bf58d4b6b0515734ffc5c80f85abd30f2de081ac
vovasl <vovasl2010@gmail.com>
2024-03-12 19:35:38 +0200

    event statistic

--- backend/services/statistic/event/total/FavoritePlayer.php
+++ backend/services/statistic/event/total/FavoritePlayer.php
@@ -1,43 +1,47 @@
 <?php
 
-namespace common\helpers\statistic\total\event\player;
+namespace backend\services\statistic\event\total;
 
+use backend\services\statistic\event\Base;
 use common\helpers\TotalHelper;
-use frontend\models\sport\Event;
-use frontend\models\sport\Odd;
 use frontend\models\sport\Round;
 use frontend\models\sport\Statistic;
-use frontend\models\sport\Surface;
 use frontend\models\sport\Tour;
 use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 
-class FavoriteHelper
+class FavoritePlayer extends Base
 {
+
+    CONST TITLE = 'Favorite';
+    CONST FAVORITE = true;
+
     /**
-     * @param Event $event
-     * @param string $type
      * @return array
      */
-    public static function getStatistic(Event $event, string $type): array
+    public function getData(): array
+    {
+        $data = parent::getData();
+        $data['favorite'] = self::FAVORITE;
+
+        return $data;
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getStatistic(): array
     {
 
-        /** empty moneyline odds */
-        if(is_null($event->favorite)) return [];
+        /** validate event */
+        if(!$this->validateEvent()) return [];
 
-        /** empty surface value */
-        if(is_null($event->eventTournament->surface)) return [];
-
-        /** search params */
-        $params = [
-            'tour' => self::getTour($event),
-            'surface' => Surface::filterValue($event->eventTournament->surface),
-            'round' => self::getRound($event),
-            'type' => $type
-        ];
+        /** get search params */
+        $params = $this->getSearchParams();
 
         /** sort - favorite is first */
-        $players = ($event->favorite == $event->home)
+        $players = ($this->event->favorite == $this->event->home)
             ? ['home', 'away']
             : ['away', 'home']
         ;
@@ -46,21 +50,48 @@ class FavoriteHelper
         $data = [];
         foreach ($players as $player) {
             $params['player'] = $player;
-            $params['favorite'] = self::getFavorite($event, $event->{$player});
-            $data[] = self::getQuery($event, $params);
+            $params['favorite'] = $this->getFavorite($player);
+            $data[] = $this->getPlayerStatistic($params);
         }
 
-        $data = self::prepare($data);
+        $data = $this->prepareStatistic($data);
 
         return $data;
     }
 
     /**
-     * @param Event $event
+     * @return bool
+     */
+    public function validateEvent(): bool
+    {
+
+        if(!parent::validateEvent()) return false;
+
+        /** empty moneyline odds */
+        if(is_null($this->event->favorite)) return false;
+
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSearchParams(): array
+    {
+        return [
+            'tour' => $this->getTour(),
+            'surface' => $this->getSurface(),
+            'round' => $this->getRound(),
+            'moneyline' => TotalHelper::MONEYLINE['over']['favorite']['max'],
+            'five_sets' => $this->event->five_sets,
+        ];
+    }
+
+    /**
      * @param array $params
      * @return array|ActiveRecord|null
      */
-    public static function getQuery(Event $event, array $params)
+    public function getPlayerStatistic(array $params)
     {
         $query = Statistic::find();
         $query->select([
@@ -82,18 +113,17 @@ class FavoriteHelper
             'event.eventTournament.tournamentTour',
             'event.eventTournament.tournamentSurface',
         ]);
-        $query = self::getWhere($query, $event, $params);
+        $query = $this->getWhere($query, $params);
 
         return $query->one();
     }
 
     /**
      * @param ActiveQuery $query
-     * @param Event $event
      * @param array $params
      * @return ActiveQuery
      */
-    public static function getWhere(ActiveQuery $query, Event $event, array $params): ActiveQuery
+    public function getWhere(ActiveQuery $query, array $params): ActiveQuery
     {
 
         /** tour */
@@ -113,13 +143,13 @@ class FavoriteHelper
         }
 
         /** event */
-        $query->andWhere(['<', 'tn_event.start_at', $event->start_at]);
-        $query->andWhere(['tn_event.five_sets' => $event->five_sets]);
+        $query->andWhere(['<', 'tn_event.start_at', $this->event->start_at]);
+        $query->andWhere(['tn_event.five_sets' => $params['five_sets']]);
 
         /** statistic */
-        $query->andWhere(['tn_statistic.player_id' => $event->{$params['player']}]);
-        $query->andWhere(['tn_statistic.add_type' => $params['type']]);
-        $query->andWhere(['<', 'tn_statistic.min_moneyline', TotalHelper::MONEYLINE['over']['favorite']['max']]);
+        $query->andWhere(['tn_statistic.player_id' => $this->event->{$params['player']}]);
+        $query->andWhere(['tn_statistic.add_type' => $this->type]);
+        $query->andWhere(['<', 'tn_statistic.min_moneyline', $params['moneyline']]);
 
         /** favorite or underdog filter */
         ($params['favorite'])
@@ -131,90 +161,74 @@ class FavoriteHelper
     }
 
     /**
-     * @param array $data
      * @return array
      */
-    public static function prepare(array $data)
-    {
-        foreach ($data as $k => $stat) {
-
-            /** unset empty statistic */
-            if($stat->count_events == 0) {
-                unset($data[$k]);
-            }
-        }
-
-        return $data;
-    }
-
-    /**
-     * @param Event $event
-     * @return array
-     */
-    public static function getPlayerUrlParams(Event $event): array
+    public function getUrl(): array
     {
         return [
             '/statistic/total/players',
-            'PlayerTotalSearch[tour]' => self::getTourFilter($event),
-            'PlayerTotalSearch[surface]' => $event->eventTournament->surface,
-            'PlayerTotalSearch[round]' => self::getRoundFilter($event),
+            'PlayerTotalSearch[tour]' => $this->getTourFilter(),
+            'PlayerTotalSearch[surface]' => $this->getSurface(),
+            'PlayerTotalSearch[round]' => $this->getRoundFilter(),
             'PlayerTotalSearch[min_moneyline]' => Statistic::TOTAL_FILTER['moneyline']['favorite'],
-            'PlayerTotalSearch[five_sets]' => $event->five_sets,
-            'PlayerTotalSearch[add_type]' => Odd::ADD_TYPE['over'],
-            'PlayerTotalSearch[favorite]' => 'Yes',
+            'PlayerTotalSearch[five_sets]' => $this->event->five_sets,
+            'PlayerTotalSearch[add_type]' => $this->type,
         ];
     }
 
     /**
-     * @param Event $event
      * @return false|int|int[]|string|null
      */
-    public static function getTour(Event $event)
+    public function getTour()
     {
-        $filterValue = self::getTourFilter($event);
+        $filterValue = $this->getTourFilter();
         if(empty($filterValue)) return false;
         return Tour::getValue($filterValue);
     }
 
     /**
-     * @param Event $event
      * @return int|string
      */
-    public static function getTourFilter(Event $event)
+    public function getTourFilter()
     {
-        if($event->round == Round::QUALIFIER) return '';
-        return Tour::getFilterValue($event->eventTournament->tour);
+        if($this->event->round == Round::QUALIFIER) return '';
+        return Tour::getFilterValue($this->event->eventTournament->tour);
     }
 
     /**
-     * @param Event $event
+     * @return int|null
+     */
+    public function getSurface(): ?int
+    {
+        return $this->event->eventTournament->surface;
+    }
+
+    /**
      * @return false|int|string
      */
-    public static function getRound(Event $event)
+    public function getRound()
     {
-        $filterValue = self::getRoundFilter($event);
+        $filterValue = $this->getRoundFilter();
         if(empty($filterValue)) return false;
         return $filterValue;
     }
 
     /**
-     * @param Event $event
      * @return int|string
      */
-    public static function getRoundFilter(Event $event)
+    public function getRoundFilter()
     {
-        if($event->round == Round::QUALIFIER) return '';
+        if($this->event->round == Round::QUALIFIER) return '';
         return Round::MAIN;
     }
 
     /**
-     * @param Event $event
-     * @param int $player
+     * @param string $player
      * @return bool
      */
-    public static function getFavorite(Event $event, int $player): bool
+    public function getFavorite(string $player): bool
     {
-        return ($player == $event->favorite);
+        return ($this->event->{$player} == $this->event->favorite);
     }
 
     /**
@@ -225,4 +239,5 @@ class FavoriteHelper
     {
         return ($key == 0 ) ? 'Yes' : 'No';
     }
+
 }
